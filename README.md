7f3e28dad5deb62d98138c665b2b3a258deb17be
Shan Chunqing <vxst@vxst.org>
2021-05-16 08:08:14 +0000

    seems to be ok

---
+++ README.md
@@ -0,0 +1,5 @@
+# CIDR merger
+
+A simple script to compress CIDR whitelists.
+
+Currently used for github action CIDR whitelist.
--- get.sh
+++ get.sh
@@ -1,1 +1,3 @@
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/meta | jq '.actions[]' | sed -e 's/"//g' 
+g++ -O2 -Wno-unused-result merge.cpp -o merge
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/meta | jq '.actions[]' | sed -e 's/"//g' | sed -e 's/^.*:.*$//g' > full_list
+./merge < full_list > simple_list
